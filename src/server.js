2b9e456109d4928c2cd9ff5189dbafedaf0e33f2
Michael Zwierzanski <69387856+mz106@users.noreply.github.com>
2023-09-25 09:40:38 +0100

    add health route

--- src/server.js
+++ src/server.js
@@ -6,6 +6,10 @@ const app = express();
 
 app.use(express.json());
 
+app.use("/health", (req, res) => {
+  res.status(200).json({ message: "API is healthy" });
+});
+
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
 });
